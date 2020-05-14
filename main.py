a99667b9ece9625171167beb30704d93565934bc
paras55 <52483274+paras55@users.noreply.github.com>
2020-05-14 15:33:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit,send
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 
 # https://flask-socketio.readthedocs.io/en/latest/
 # https://github.com/socketio/socket.io-client
 
 app = Flask(__name__)
-run_with_ngrok(app) 
+#run_with_ngrok(app) 
 
 app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
-socketio = SocketIO( app )
+#socketio = SocketIO( app )
 
 @app.route( '/' )
 def hello():
@@ -31,6 +31,6 @@ def handleMessage(msg):
 	#send(ok, broadcast=True) # this ok is printed in chatbox , whatever you send from here is printed in chat'''
 
 if __name__ == '__main__':
-  app.run()
-  socketio.run( app, debug = True )
+  app.run(debug = True)
+  #socketio.run( app, debug = True )
   
